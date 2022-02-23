1a67b0baf09d8f8b1364ff0512aa44f8bb763fa9
roaminroe <roamin@tuta.io>
2022-02-23 18:38:02 -0500

    ROCKSDB_BUILD_SHARED=ON

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -26,7 +26,7 @@ hunter_config(rocksdb
       WITH_GFLAGS=OFF
       PORTABLE=ON
       FAIL_ON_WARNINGS=OFF
-      ROCKSDB_BUILD_SHARED=OFF
+      ROCKSDB_BUILD_SHARED=ON
       CMAKE_CXX_FLAGS=-fvisibility=hidden
       CMAKE_C_FLAGS=-fvisibility=hidden
 )
