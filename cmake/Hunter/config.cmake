b0b0156485cbafdd8962782c53cf00746402de00
roaminroe <roamin@tuta.io>
2022-03-03 19:50:01 -0500

    test windows

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -10,9 +10,14 @@ hunter_config(Protobuf
    URL  "https://github.com/koinos/protobuf/archive/e1b1477875a8b022903b548eb144f2c7bf4d9561.tar.gz"
    SHA1 "5796707a98eec15ffb3ad86ff50e8eec5fa65e68"
    CMAKE_ARGS
-      CMAKE_CXX_FLAGS=-fvisibility=hidden
-      CMAKE_C_FLAGS=-fvisibility=hidden
+      #CMAKE_CXX_FLAGS=-fvisibility=hidden
+      #CMAKE_C_FLAGS=-fvisibility=hidden
       protobuf_BUILD_SHARED_LIBS=ON
+      protobuf_WITH_ZLIB=OFF
+      protobuf_BUILD_PROTOC_BINARIES=OFF
+      protobuf_BUILD_LIBPROTOC=OFF
+      protobuf_BUILD_TESTS=OFF
+      protobuf_BUILD_EXAMPLES=OFF
 )
 
 hunter_config(rocksdb
